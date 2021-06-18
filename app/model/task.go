3386e707b7a9b22e642c6a991e3a0122b6b84895
lil-shimon <kenta5006>
2021-06-18 12:38:00 +0900

    fix

--- app/model/task.go
+++ app/model/task.go
@@ -4,5 +4,7 @@ type Task struct {
 	Id int `json:"id" gorm:"primary_key;AUTO_INCREMENT" param:"id"`
 	CategoryId int `json:"category_id"`
 	Name string `json:"name"`
-	Content string `json:"content"`
+	Content *string `json:"content"`
 }
+
+type Tasks []*Task
