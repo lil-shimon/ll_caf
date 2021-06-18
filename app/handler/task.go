07fb2182b58bd41d40c8655206d4308fddcc21db
lil-shimon <kenta5006>
2021-06-18 12:43:00 +0900

    fix

--- app/handler/task.go
+++ app/handler/task.go
@@ -1,1 +1,7 @@
 package handler
+
+import "github.com/lil-shimon/workManager/model"
+
+func GetTasks() error {
+	t, _ := repo
+}
--- app/regository/task.go
+++ app/regository/task.go
@@ -1,1 +1,15 @@
 package regository
+
+import (
+	"github.com/lil-shimon/workManager/database"
+	"github.com/lil-shimon/workManager/model"
+)
+
+func GetRepoTask() (model.Tasks, error) {
+	t := model.Tasks{}
+
+	if err := database.DB.Find(&t).Error; err != nil {
+		return t, err
+	}
+	return t, nil
+}
