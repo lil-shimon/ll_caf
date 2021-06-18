14407e67df3d4fc4c0222e0bfd8aff3dd0708b38
lil-shimon <kenta5006>
2021-06-18 12:45:00 +0900

    fix

--- app/handler/task.go
+++ app/handler/task.go
@@ -3,9 +3,10 @@ package handler
 import (
 	"github.com/lil-shimon/workManager/model"
 	"github.com/lil-shimon/workManager/repository"
+	"net/http"
 )
 
 func GetTasks() error {
 	t, _ := repository.GetRepoTasks()
-	return t
+	return c.JSON(http.StatusOK, t)
 }
