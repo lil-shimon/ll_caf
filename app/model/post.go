9051c2f9a33163321cafbf51782a002604a8ca37
shimon <69175188+lil-shimon@users.noreply.github.com>
2021-10-19 17:56:29 +0900

    fix

--- app/model/post.go
+++ app/model/post.go
@@ -4,8 +4,8 @@ import "gorm.io/gorm"
 
 type Post struct {
 	gorm.Model
-	Content string `json:content`
-	UserId  int    `json:user_id`
+	Content string `json:"content"`
+	UserId  int    `json:"user_id"`
 }
 
 type Posts []Post
--- app/model/user.go
+++ app/model/user.go
@@ -4,11 +4,11 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	Name     string `json:name`
-	Email    string `json:email`
-	Password string `json:password`
-	Bio      string `json:bio`
-	contact  string `json:contact`
-	Website  string `json:website`
-	Image    string `json:image`
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+	Bio      string `json:"bio"`
+	Contact  string `json:"contact"`
+	Website  string `json:"website"`
+	Image    string `json:"image"`
 }
