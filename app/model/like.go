8844d610565807c50acc58e556a4e0c463b4be2b
shimon <69175188+lil-shimon@users.noreply.github.com>
2021-10-20 12:43:13 +0900

    add like model

---
+++ app/model/like.go
@@ -0,0 +1,10 @@
+package model
+
+import "time"
+
+type Like struct {
+	Id        uint      `json:"id"`
+	UserId    int       `json:"user_id"`
+	PostId    int       `json:"post_id"`
+	CreatedAt time.Time `json:"created_at"`
+}
